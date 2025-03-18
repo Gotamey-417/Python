dbe7b7a013f4a3b1e5338e8a2d58307193668acc
Sandip <sandipgautam263@gmail.com>
2025-03-18 07:54:41 +0545

    Var

---
+++ var,datatype.py
@@ -0,0 +1,22 @@
+s = 7
+m = "python"
+print(m)
+print(s) 
+
+x=65
+x="silly"
+print(x)
+
+#python has no command for declaration
+
+#data type
+
+a= 3
+print(type(a))
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+print(type(x))
+print(type(y))
+print(type(z))
