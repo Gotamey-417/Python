42c4def4c56585eb23569763493007137461429f
Sandip <sandipgautam263@gmail.com>
2025-03-20 06:37:23 +0545

    Python Operator

---
+++ Operator.py
@@ -0,0 +1,64 @@
+# type: ignore
+
+  # Operators are used to perform operations on variables and values.
+
+# Arithmetic operators
+
+# a=6
+# y=5
+# sum=(a+y)
+# sub=(a-y)
+# mul=(a*y)
+# div=(a/y)
+
+# print(sum)
+# print(sub)
+# print(mul)
+# print(div)
+
+
+# Assignment operators are used to assign values to variables:
+
+
+# 1. Basic Assignment (=)
+# This assigns a value to a variable:
+
+# x = 10
+# print(x)
+
+
+# 2. Add and Assign (+=)
+# Adds a value to the current value of the variable and assigns the result back:
+
+# x = 5
+# x += 3  
+# print(x)
+
+
+# 3. Subtract and Assign (-=)
+# # Subtracts a value from the current value and assigns the result:
+
+# x = 10
+# x -= 4  
+# print(x) 
+
+
+
+
+# # Bitwise AND Assign (&=)
+# # Performs a bitwise AND operation and assigns the result:
+
+
+# x = 6  
+# x &= 3 
+# print(x)
+
+
+
+
+# Bitwise OR Assign (|=)
+# Performs a bitwise OR operation and assigns the result:
+
+# x = 4 
+# x |= 1  
+# print(x)  
